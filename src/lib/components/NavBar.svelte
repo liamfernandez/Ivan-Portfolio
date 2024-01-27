08b569ebc3bf164183af4f260b1a04796d0cf64d
Liam Fernandez <liamdfernandez@gmail.com>
2024-01-27 15:27:20 -0500

    small changes

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -3,29 +3,32 @@
 
 	export let currentPage: string;
 
-	const bannerMap = {
-		'/': 'HOME',
-		'/research': 'RESEARCH',
-		'/teaching': 'TEACHING',
-		'/resume': 'CV',
-		'/contact': 'CONTACT'
-	};
+	const bannerMap = new Map<string, string>([
+		['/', 'home.png'],
+		['/research', 'research.png'],
+		['/teaching', 'teaching.png'],
+		['/resume', 'cv.png'],
+		['/contact', 'contact.png']
+	]);
 </script>
 
-<nav class="w-full font-myanmar flex justify-between items-center flex-row pt-10 px-16">
-	<a class="text-[1.45rem]" href="/">
+<img class=" -z-10 -mt-20 absolute" src={`/banners/${bannerMap.get(currentPage)}`} />
+<nav
+	class=" z-10 w-full text-white font-myanmar flex justify-between items-center flex-row pt-10 px-16"
+>
+	<a class="text-[1.65rem]" href="/">
 		<span class="flex flex-row items-center">
 			<span class="">IV</span>
-			<span class="text-[1.4rem]">Á</span>
+			<span class="text-[1.6rem]">Á</span>
 			<span class="">N</span>
 		</span>
 		<span class="flex flex-row items-center">
 			<span class="">RUIZ-HERN</span>
-			<span class="text-[1.4rem]">Á</span>
+			<span class="text-[1.6rem]">Á</span>
 			<span class="">NDEZ</span>
 		</span>
 	</a>
-	<ul class="flex flex-row gap-10 text-lg">
+	<ul class="flex flex-row gap-20 text-xl">
 		<li><a href="/research">RESEARCH</a></li>
 		<li><a href="/teaching">TEACHING</a></li>
 		<li><a href="/resume">CV</a></li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+	<title>Iván Ruiz-Hernández | PhD Candidate</title>
+</svelte:head>
+
 <h1 class="mt-20 font-myanmar">Welcome to SvelteKit</h1>
 <p class=" font-bangala">
 	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
